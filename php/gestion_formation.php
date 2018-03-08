fe3f54d106f17259e8daed3b0024f6b72bf567ba
Flpichon <33513602+Flpichon@users.noreply.github.com>
2018-03-08 09:32:43 +0100

    creation d'une page gestion_formation

---
+++ php/gestion_formation.php
@@ -0,0 +1,27 @@
+<?php
+include_once 'test.php';
+EstConnecte();
+if(Estmanager())
+{
+include '../boot_css/header_man.php';
+}
+else include '../boot_css/header.php';
+$credit=CreditEmploye();
+?>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<title>Gestion des formations</title>
+</head>
+<body style="background:#E8E7E7">
+<div class="alert alert-primary d-flex justify-content-between" role="alert">
+<?php echo '<div>Connecté en tant que'."  ".$_SESSION['Prenom_Employe']." ".$_SESSION['nom_Employe']."</div>"
+		   ;?>
+	<div>Il vous reste <?echo $credit['credit'];?> crédits</div>
+</div>
+
+</body>
